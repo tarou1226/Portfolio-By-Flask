7e7dfe0589476cf7771a4ed2f31a2808f6fc7271
tarou <teroro1226@outlook.jp>
2022-12-21 14:42:07 +0000

    [create] make log

---
+++ log.py
@@ -0,0 +1,13 @@
+import os
+from datetime import datetime
+
+def make_log_dir():
+    if not os.path.exists("./log"):
+        os.makedirs("./log")
+
+def write_log(log):
+    today = datetime.now()
+    file_path = "./log/" + today.strftime("%Y年%m月%d日") + ".txt"
+    f = open(file_path, mode="x")
+    f.writelines(log)
+    f.close()
