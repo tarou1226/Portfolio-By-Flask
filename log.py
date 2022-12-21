8012a8a7adc58131299f83c132c04afceb955696
tarou <teroro1226@outlook.jp>
2022-12-21 14:55:38 +0000

    [fix] set to be able to add

--- log.py
+++ log.py
@@ -8,6 +8,7 @@ def make_log_dir():
 def write_log(log):
     today = datetime.now()
     file_path = "./log/" + today.strftime("%Y年%m月%d日") + ".txt"
-    f = open(file_path, mode="x")
+    f = open(file_path, mode="a")
     f.writelines(log)
+    f.write("\n")
     f.close()
